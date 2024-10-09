8adbe98730a1ff659e1a07802dc25d9d2a0d7297
Pedro Gusmao <padg@cesar.school>
2024-10-09 07:41:45 -0300

    Uptade Model Material

--- projeto_django/software_g17/estoque/models.py
+++ projeto_django/software_g17/estoque/models.py
@@ -4,6 +4,7 @@ class Material(models.Model) :
     material = models.CharField(max_length=20)
     quantidade = models.IntegerField()
     preco = models.FloatField()
+    date_added = models.DateTimeField(null=True)
 
     def __str__(self):
         return self.material
