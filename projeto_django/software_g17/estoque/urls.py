8d8e5a0c5564af4396754b7b48612efbb79096bf
Pedro Gusmao <padg@cesar.school>
2024-10-09 18:32:56 -0300

    Uptade Materiais

--- projeto_django/software_g17/estoque/urls.py
+++ projeto_django/software_g17/estoque/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import login_view, cadastro_view, home_view
+from .views import login_view, cadastro_view, home_view, estoque_view
 
 urlpatterns = [
     path('', login_view, name='login'),
     path('cadastro/', cadastro_view, name='cadastro'),
     path('home/', home_view, name='home'),
-
+    path('estoque/', estoque_view, name='estoque'),
 ]
--- projeto_django/software_g17/estoque/views.py
+++ projeto_django/software_g17/estoque/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth import authenticate, login
+from .models import Material
 
 def login_view(request) :
     if request.method == 'POST':
@@ -20,3 +21,23 @@ def home_view(request) :
     fantoches = 0
     context = {"qnt_materias" : qnt_materias, "fantoches" : fantoches}
     return render(request, 'estoque/home.html', context)
+
+def estoque_view(request) :
+    if request.method == 'POST' :
+        material = request.POST.get('material')
+        quantidade = request.POST.get('quantidade')
+        preco = request.POST.get('preco')
+        #date_added = request.POST.get('preco')
+
+        M = Material(
+            material = material,
+            quantidade = quantidade,
+            preco = preco,
+        )
+        M.save()
+
+    materiais = Material.objects.all()
+
+    context = {'materiais' : materiais}
+
+    return render(request, 'estoque/estoque.html', context)
