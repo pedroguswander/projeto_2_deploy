662903523a1ea184077b5ddbd1b20b51c3597dba
Pedro Gusmao <padg@cesar.school>
2024-10-09 07:36:04 -0300

    Adicionando model material

--- projeto_django/software_g17/estoque/models.py
+++ projeto_django/software_g17/estoque/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Material(models.Model) :
+    material = models.CharField(max_length=20)
+    quantidade = models.IntegerField()
+    preco = models.FloatField()
+
+    def __str__(self):
+        return self.material
