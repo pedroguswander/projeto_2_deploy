390c28c3a23516d9c96585acb3db964e22cc62ba
Pedro Gusmao <padg@cesar.school>
2024-10-17 19:53:40 -0300

    Alterando o caminho principal para brinquedos

--- projeto_django/estoque/urls.py
+++ projeto_django/estoque/urls.py
@@ -3,10 +3,10 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', login_view, name='login'),
+    path('login', login_view, name='login'),
     path('cadastro/', cadastro_view, name='cadastro'),
     path('home/', home_view, name='home'),
-    path('brinquedos/', brinquedos_view, name='brinquedos'),
+    path('', brinquedos_view, name='brinquedos'),
     path('brinquedos_cadastro/', brinquedos_cadastro_view, name='brinquedos_cadastro'),
     path('brinquedo/<brinquedo_id>/', brinquedo_view, name='brinquedo'),
     path('estoque/', estoque_view, name='estoque'),
